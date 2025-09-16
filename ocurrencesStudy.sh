0bbf0f90804e2fd7d63f338d4e5297406ac1f873
Jaiver <salazar_64@live.com>
2025-09-15 20:06:09 -0500

    feature: added ocurrencesstudy.sh

---
+++ ocurrencesStudy.sh
@@ -0,0 +1,47 @@
+./grid_analysis 3 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,3\)
+
+./grid_analysis 4 10 1.0
+rootj-b -q statsMacroChi2LRSO3Sn.C\(10,1,4\)
+
+./grid_analysis 5 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,5\)
+
+./grid_analysis 6 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,6\)
+
+./grid_analysis 7 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,7\)
+
+./grid_analysis 8 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,8\)
+
+./grid_analysis 9 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,9\)
+
+./grid_analysis 10 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,10\)
+
+./grid_analysis 11 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,11\)
+
+./grid_analysis 12 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,12\)
+
+./grid_analysis 13 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,13\)
+
+./grid_analysis 14 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,14\)
+
+./grid_analysis 15 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,15\)
+
+./grid_analysis 17 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,17\)
+
+./grid_analysis 19 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,19\)
+
+./grid_analysis 21 10 1.0
+root -b -q statsMacroChi2LRSO3Sn.C\(10,1,21\)
